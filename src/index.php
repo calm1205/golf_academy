272aa24a054c71052f2fa07eb50027b04ed85c55
calm1205 <naginani072@gmail.com>
2023-04-02 22:03:03 +0900

    update

--- src/index.php
+++ src/index.php
@@ -7,7 +7,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>ゴルフレッスンサイト</title>
   <link rel="stylesheet" href="./dist/output.css" />
-  <!-- <link rel="stylesheet" href="./index.css" /> -->
 </head>
 
 <body>

afb2f062b365478d90864ac72c30d2847cdb1a86
calm1205 <naginani072@gmail.com>
2023-04-02 22:01:09 +0900

    install tailwind

--- src/index.php
+++ src/index.php
@@ -6,10 +6,12 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>ゴルフレッスンサイト</title>
+  <link rel="stylesheet" href="./dist/output.css" />
+  <!-- <link rel="stylesheet" href="./index.css" /> -->
 </head>
 
 <body>
-  <h1>hello world</h1>
+  <h1 class="text-3xl font-bold underline">hello world.</h1>
   <?php include './header/header.php'; ?>
   <?php include './footer.php'; ?>
 </body>

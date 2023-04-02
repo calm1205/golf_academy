9d06a51354d83b26bc536e02e4ba8ae6aef3c1bb
calm1205 <naginani072@gmail.com>
2023-04-02 19:55:20 +0900

    add src

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ゴルフレッスンサイト</title>
+</head>
+<body>
+    <?php include('./header.php') ?>
+    <?php include('./content.php') ?>
+</body>
+</html>

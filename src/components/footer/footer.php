ebfd716db7c9246435fd70a9acfe21c0c8649ef1
osatou <satohirosnorlax0213@gmail.com>
2023-04-14 10:58:30 +0900

    main

--- src/components/footer/footer.php
+++ src/components/footer/footer.php
@@ -1,5 +1,5 @@
-<footer class="bottom-0  w-full bg-sky-800">
+<!-- <footer class="bottom-0  w-full bg-sky-800">
   <div class="bottom-0 h-8 w-full bg-sky-950 text-white text-xs text-center p-2 font-light">
     copy light golf studio
   </div>
-</footer>
+</footer> -->

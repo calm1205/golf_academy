36623c7f453a9bfeabf0a345c9c423001411c0eb
satohiro213 <satohirosnorlax0213@gmail.com>
2023-04-15 02:42:55 +0900

    feature/footer

--- src/components/footer/copyright.php
+++ src/components/footer/copyright.php
@@ -1,3 +1,4 @@
-<footer class="bottom-0  w-full bg-sky-800 text-white text-xs text-center p-2 font-light">
-  <p>&copy; 2021-2023 After Golf Studio. All Rights Reserved.</p>
-</footer>
+  <div class="bottom-0  w-full bg-sky-800 text-white text-xs text-center p-2 font-light h-12 flex justify-center items-center">
+    <p>&copy; 2021-2023 After Golf Studio. All Rights Reserved.</p>
+  </div>
+  </footer>

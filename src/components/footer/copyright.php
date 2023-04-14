6c14c0652673d3e06f26ddf86f923d7560f3fffb
osatou <satohirosnorlax0213@gmail.com>
2023-04-14 10:57:27 +0900

    main

--- src/components/footer/copyright.php
+++ src/components/footer/copyright.php
@@ -1,5 +1,5 @@
-<footer class="fixed bottom-0 w-full bg-sky-800">
-  <div class="fixed bottom-0 h-8 w-full bg-sky-800 text-white text-xs text-center p-2 font-light">
+<footer class="bottom-0 h-8 w-full bg-sky-800">
+  <div class="bottom-0  w-full bg-sky-800 text-white text-xs text-center p-2 font-light">
     <p>&copy; 2021-2023 After Golf Studio. All Rights Reserved.</p>
   </div>
 </footer>

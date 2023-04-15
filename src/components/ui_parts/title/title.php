2d4859aab87ae5de2a96b9ab372d656043d0fcda
calm1205 <naginani072@gmail.com>
2023-04-15 23:21:50 +0900

    colorの変更

--- src/components/ui_parts/title/title.php
+++ src/components/ui_parts/title/title.php
@@ -1,7 +1,7 @@
 <!-- ページ内の青グラデーション背景のタイトル -->
 
-<div class="py-3 flex justify-center items-center text-[40px] text-white font-bold bg-gradient-to-r from-primary-light from-34% to-primary-default border-b-[3px] border-solid border-primary-dark">
+<div class="py-3 flex justify-center items-center bg-gradient-to-r from-primary-light from-34% to-primary-default border-b-[3px] border-solid border-primary-dark">
   <?php include __DIR__ . '/line.php' ?>
-  <h2 class="inline-block"><?php echo $title ?></h2>
+  <h2 class="inline-block text-[40px] text-white font-bold"><?php echo $title ?></h2>
   <?php include __DIR__ . '/line.php' ?>
 </div>

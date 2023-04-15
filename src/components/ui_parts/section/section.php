fe114d670e12639a58647c68e190662d06f85126
calm1205 <naginani072@gmail.com>
2023-04-15 23:20:40 +0900

    sectionコンポーネントの実装

---
+++ src/components/ui_parts/section/section.php
@@ -0,0 +1,12 @@
+<!-- タイトル付きセクション -->
+
+<!-- 
+  title: タイトル
+  children: コンテンツ
+ -->
+
+<?php include $uiPartsPath . '/title/title.php'; ?>
+
+<div class="w-4/5 mx-auto m-10">
+  <?php echo $children ?>
+</div>

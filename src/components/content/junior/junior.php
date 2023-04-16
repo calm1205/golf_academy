4e96aa782929be7bcc33e897deb1c5b44d75e1b3
Moriyama Nagi <naginani072@gmail.com>
2023-04-16 11:19:34 +0900

    Feature/content (#27)
    <!-- #の直後にIssue番号 -->
    Close #
    
    ## 概要
    
    ## やったこと
    
    ## やらなかったこと
    
    ## 備考
    <!-- 参考にした資料や記事あれば -->

--- src/components/content/junior/junior.php
+++ src/components/content/junior/junior.php
@@ -1,3 +1,11 @@
-<div class="h-96 flex justify-center items-center">
-  [未]
+<div class="flex justify-center items-center gap-10">
+  <img src="./asset/junior/junior.png" />
+
+  <div class="flex flex-col">
+    <p class="text-base whitespace-pre-line">
+      ゴルフの基本動作の習得から、スコアを伸ばすための本格的なゴルフレッスンまで指導可能です。
+
+      ゴルフを楽しみたいお子さまも、プロを目指してハイレベルのレッスンを受けたいお子さまにも、それぞれに合った個別指導をおこなっています。
+    </p>
+  </div>
 </div>

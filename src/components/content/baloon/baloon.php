42b3091632a7a39849e01c7c25c18dd1fb75c646
satohiro213 <satohirosnorlax0213@gmail.com>
2023-04-16 14:09:27 +0900

    feature/baloon

--- src/components/content/baloon/baloon.php
+++ src/components/content/baloon/baloon.php
@@ -1,5 +1,5 @@
-<div>
-  <p>
+<div class="text-center py-4 px-6 border border-blue-400 w-max text-2xl font-bold inline-block relative">
+  <p class="">
     ゴルフをこれから始める人も！<br>もっと上達したい人も！ 大歓迎！
   </p>
 </div>

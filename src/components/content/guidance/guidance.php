2857dff07248b104fb5b64c87a02d8c3ceed89f7
satohiro213 <satohirosnorlax0213@gmail.com>
2023-04-16 15:23:22 +0900

    feature/guidance

--- src/components/content/guidance/guidance.php
+++ src/components/content/guidance/guidance.php
@@ -3,12 +3,12 @@
   <table class="border-b">
     <tr class="border-t">
       <th class="font-normal py-3">
-        <div class="h-4 border-r m-0 p-0 pr-2 leading-4">営業時間</div>
+        <div class="h-4 border-r m-0 p-0 leading-4 pr-2">営業時間</div>
       </th>
       <td class="pl-2">10:00〜21:00</td>
     </tr>
     <tr class="border-t">
-      <th class="font-normal py-3 ">
+      <th class="font-normal py-3">
         <div class="h-4 border-r m-0 p-0 pr-2 leading-4">駐車場</div>
       </th>
       <td class="pl-2">5台</td>

558a9b0163984ea0246a5696b7b18530cfad95b5
satohiro213 <satohirosnorlax0213@gmail.com>
2023-04-16 19:10:59 +0900

    feature/button

--- src/components/content/baloon/baloon.php
+++ src/components/content/baloon/baloon.php
@@ -1,5 +1,13 @@
-<div class="text-center py-4 px-6 border border-blue-400 w-max text-2xl font-bold inline-block relative">
-  <p class="">
-    ゴルフをこれから始める人も！<br>もっと上達したい人も！ 大歓迎！
-  </p>
+<div class="m-0 text-center p-0 relative
+before:content-[''] before:absolute before:w-16 before:h-16 before:border-t before:border-blue-400 before:-rotate-45 before:origin-bottom-left before:translate-x-8 before:translate-y-4
+after:content-[''] after:absolute after:top-full after:w-16 after:h-16  after:border-t after:border-blue-400 after:-rotate-45 after:origin-bottom-left after:translate-x-3 after:-translate-y-1">
+  <div class="text-center py-4 px-6 box-border border border-blue-400 w-auto text-2xl font-bold inline-block relative shadow-[4px_4px_0_0_rgba(66,165,245,1)] 
+  before:content-[''] before:w-0 before:h-0 before:absolute before:top-full before:left-1/2 before:border-32 before:-translate-x-1/2 before:border-blue-400 before:border-r-transparent before:border-b-transparent before:border-l-transparent  
+  after:content-[''] after:w-0 after:h-0 after:absolute after:top-full after:left-1/2 after:border-30 after:-translate-x-1/2 after:border-white after:border-r-transparent after:border-b-transparent after:border-l-transparent">
+    <p class="m-0 p-0 relative 
+    before:content-[''] before:absolute before:top-full before:left-1/2 before:w-12 before:h-12 before:border-l-4 before:border-blue-400 before:rotate-45 before:origin-bottom-left 
+    after:content-[''] after:absolute after:top-full after:left-1/2 after:w-24 after:h-4 after:bg-white after:-translate-x-1/2">
+      ゴルフをこれから始める人も！<br>もっと上達したい人も！ 大歓迎！
+    </p>
+  </div>
 </div>

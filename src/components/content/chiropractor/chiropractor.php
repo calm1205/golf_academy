d2516d21017889947bb53b487847d7f2b671094d
calm1205 <naginani072@gmail.com>
2023-04-16 11:18:50 +0900

    chiropractor

--- src/components/content/chiropractor/chiropractor.php
+++ src/components/content/chiropractor/chiropractor.php
@@ -1,3 +1,10 @@
-<div class="h-96 flex justify-center items-center">
-  [未]
+<div class="flex justify-center items-center gap-10">
+  <img src="./asset/chiropractor/chiropractor.png" />
+
+  <div class="flex flex-col">
+    <p class="text-base whitespace-pre-line">
+      骨盤や股関節、肩甲骨を重点 的に施術し、ゴルフを正しい 動作でプレーするための身体 作りをサポートします。
+      ゴルフに使用する様々な筋肉 をケアすることで、飛距離の 向上や、疲れにくい身体づく りをすることが可能です。
+    </p>
+  </div>
 </div>

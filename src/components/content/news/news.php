99eb8359b6e76700fb6b5169ecba9ebb0b1117cc
satohiro213 <satohirosnorlax0213@gmail.com>
2023-04-18 19:07:31 +0900

    feature/news

--- src/components/content/news/news.php
+++ src/components/content/news/news.php
@@ -3,14 +3,14 @@
   <section class="w-4/5 mt-2 flex flex-col gap-4">
     <?php for ($i = 0; $i <= 10; $i++) { ?>
       <a href="#">
-        <article class="flex gap-2 border-b pb-5">
+        <article class="flex gap-6 border-b pb-5 items-center">
           <span class="text-[8px] text-white bg-sky-600 px-2 w-fit h-4">
             2022/10/10
           </span>
-          <p class="text-base text-gray-600">
+          <p class="text-base text-gray-600 tracking-wider font-extralight">
             ニュース仮テキスト〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇
           </p>
-          <i class="fa-solid fa-chevron-right"></i>
+          <i class="fa-solid fa-chevron-right text-gray-600 font-extralight"></i>
 
         </article>
       </a>

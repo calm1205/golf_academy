582c338b4f060c9a9cfb01ab231cff0aeeb73560
satohiro213 <satohirosnorlax0213@gmail.com>
2023-04-18 19:55:33 +0900

    feature/guidance

--- src/components/content/guidance/guidance.php
+++ src/components/content/guidance/guidance.php
@@ -1,6 +1,6 @@
 <h2 class="text-base font-bold pb-2 ">営業のご案内</h2>
 <div class="text-left">
-  <table class="border-b">
+  <table class="border-b font-normal">
     <tr class="border-t">
       <th class="font-normal py-3">
         <div class="h-4 border-r m-0 p-0 leading-4 pr-2">営業時間</div>
@@ -14,7 +14,7 @@
       <td class="pl-2">5台</td>
     </tr>
     <tr class="border-t">
-      <th class="font-normal py-3 ">
+      <th class="font-normal py-3">
         <div class="h-4 border-r m-0 p-0 pr-2 leading-4">定休日</div>
       </th>
       <td class="pl-2">日曜日・祝日</td>

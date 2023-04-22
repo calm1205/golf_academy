ba6d84c4a36abc5ce2363fff51ca786df89d9419
calm1205 <naginani072@gmail.com>
2023-04-22 16:52:42 +0900

    レイアウト調整

--- src/components/content/news/news.php
+++ src/components/content/news/news.php
@@ -1,18 +1,20 @@
 <!-- 記事 -->
 <div class="flex justify-center mt-10">
-  <section class="w-4/5 mt-2 flex flex-col gap-4">
+  <section class="w-4/5 max-h-80 mt-2 flex flex-col overflow-y-scroll">
     <?php for ($i = 0; $i <= 10; $i++) { ?>
-      <a href="#">
-        <article class="flex gap-6 border-b pb-5 items-center">
-          <span class="text-[8px] text-white bg-sky-600 px-2 w-fit h-4">
+      <a href="#" class="flex justify-between items-center border-b p-3">
+        <article class="flex gap-6 w-full">
+          <span class="text-[8px] text-white bg-sky-600 p-1">
             2022/10/10
           </span>
-          <p class="text-base text-gray-600 tracking-wider font-extralight">
+          <p class="max-w-[80%] text-base text-gray-600 tracking-wider font-extralight text-ellipsis whitespace-nowrap overflow-hidden">
+            ニュース仮テキスト〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇
+            ニュース仮テキスト〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇
             ニュース仮テキスト〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇
           </p>
-          <i class="fa-solid fa-chevron-right text-gray-600 font-extralight"></i>
-
         </article>
+
+        <i class="fa-solid fa-chevron-right text-gray-600 font-extralight"></i>
       </a>
     <?php } ?>
   </section>

bbfe37974382fc7f3ebc8f80d7fb0bd53d7b903b
calm1205 <naginani072@gmail.com>
2023-04-22 13:12:24 +0900

    styleの修正

--- src/components/content/guidance/guidance.php
+++ src/components/content/guidance/guidance.php
@@ -2,25 +2,23 @@
   営業のご案内
 </h2>
 
-<div class="text-left">
-  <table class="border-b [&_th]:font-normal [&_th]:py-3 [&_td]:pl-2 [&_tr]:border-t">
-    <tr>
-      <th>
-        <div class="h-4 border-r m-0 p-0 leading-4 pr-2">営業時間</div>
-      </th>
-      <td>10:00〜21:00</td>
-    </tr>
-    <tr>
-      <th>
-        <div class="h-4 border-r m-0 p-0 pr-2 leading-4">駐車場</div>
-      </th>
-      <td>5台</td>
-    </tr>
-    <tr>
-      <th>
-        <div class="h-4 border-r m-0 p-0 pr-2 leading-4">定休日</div>
-      </th>
-      <td>日曜日・祝日</td>
-    </tr>
-  </table>
-</div>
+<table class="text-left border-b [&_th]:font-normal [&_th]:py-3 [&_td]:pl-2 [&_tr]:border-t [&_div]:border-r [&_div]:pr-2">
+  <tr>
+    <th>
+      <div>営業時間</div>
+    </th>
+    <td>10:00〜21:00</td>
+  </tr>
+  <tr>
+    <th>
+      <div>駐車場</div>
+    </th>
+    <td>5台</td>
+  </tr>
+  <tr>
+    <th>
+      <div>定休日</div>
+    </th>
+    <td>日曜日・祝日</td>
+  </tr>
+</table>

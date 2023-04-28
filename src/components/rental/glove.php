26d4b7c830455cd2d556c3aebc0bf066f03521ba
satohiro213 <satohirosnorlax0213@gmail.com>
2023-04-29 03:35:15 +0900

    "画像をassetに配置、ui_partsのsection.phpをincludeして配置"

---
+++ src/components/rental/glove.php
@@ -0,0 +1,11 @@
+<div class="flex justify-start items-center gap-10">
+  <img src="<?php echo $absoluteUriPath ?>/asset/rental/glove.png" />
+
+  <div class="flex flex-col">
+    <p class="text-base whitespace-pre-line">
+      レンタルグローブは男性用のほかレディースサイズとジュニアサイズもご用意。
+      グローブを忘れてしまった、レッスン中に破れてしまったなどのトラブルがあった際は
+      ご利用ください。
+    </p>
+  </div>
+</div>
--- src/components/rental/rental.php
+++ src/components/rental/rental.php
@@ -1,1 +1,9 @@
-<?php include $absoluteIncludePath . '/components/rental/header.php';
+<?php include $absoluteIncludePath . '/components/rental/header.php'; ?>
+
+<?php
+$title = 'レンタルグローブ';
+ob_start();
+include $absoluteIncludePath . '/components/rental/glove.php';
+$children = ob_get_clean();
+include $uiPartsPath . '/section/section.php'
+?>

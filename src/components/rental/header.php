ec6f88d5864f40ea5581fa1716ffdc183ac096ed
satohiro213 <satohirosnorlax0213@gmail.com>
2023-04-29 03:58:51 +0900

    "section.phpをincludeして画像と共に配置"

--- src/components/rental/header.php
+++ src/components/rental/header.php
@@ -1,3 +1,3 @@
-<img src="<?php echo $absoluteUriPath ?>/asset/rental/header.svg" class="w-full">
+<img src="<?php echo $absoluteUriPath ?>/asset/rental/shoes.svg" class="w-full">
 
 <p class="mt-4 text-center text-xl tracking-wide">ゴルフレッスンを手ぶらでも受けられるように、無料のレンタルクラブをご用意しております。<br>ゴルフクラブをお持ちでない方、身軽にレッスンを受講したい方はご利用ください。</p>
--- src/components/rental/rental.php
+++ src/components/rental/rental.php
@@ -1,6 +1,14 @@
 <?php include $absoluteIncludePath . '/components/rental/header.php'; ?>
 
 <?php
+$title = 'レンタルシューズ';
+ob_start();
+include $absoluteIncludePath . '/components/rental/shoes.php';
+$children = ob_get_clean();
+include $uiPartsPath . '/section/section.php'
+?>
+
+<?php
 $title = 'レンタルグローブ';
 ob_start();
 include $absoluteIncludePath . '/components/rental/glove.php';

bb8577ebade9580f6250d57402db21beb0fd8325
satohiro213 <satohirosnorlax0213@gmail.com>
2023-04-30 04:38:02 +0900

    "guidanceTableとGoogleMapのスタイル調整"

--- src/components/content/guidance/guidanceTable.php
+++ src/components/content/guidance/guidanceTable.php
@@ -1,9 +1,9 @@
-<div>
+<div class="phone:ml-4">
   <h2 class="text-base font-bold pb-2">
     営業のご案内
   </h2>
 
-  <table class="text-left border-b [&_th]:font-normal [&_th]:py-3 [&_td]:pl-2 [&_tr]:border-t [&_div]:border-r [&_div]:pr-2">
+  <table class="phone:w-[calc(100%-32px)] text-left border-b [&_th]:font-normal [&_th]:py-3 [&_td]:pl-2 [&_tr]:border-t [&_div]:border-r [&_div]:pr-2 ">
     <tr>
       <th>
         <div>営業時間</div>

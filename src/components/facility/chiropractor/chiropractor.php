34a144d4f479a6fc58b0fe451029866225742820
calm1205 <naginani072@gmail.com>
2023-04-29 12:25:51 +0900

    ゴルフ整体・画像と文章

--- src/components/facility/chiropractor/chiropractor.php
+++ src/components/facility/chiropractor/chiropractor.php
@@ -1,1 +1,10 @@
-<h1>ゴルフ整体</h1>
+<div class="flex justify-start items-center gap-10">
+  <img src="<?php echo $absoluteUriPath ?>/asset/chiropractor/chiropractor.png" />
+
+  <div class="flex flex-col">
+    <p class="text-base whitespace-pre-line">
+      After Golf Studioは整体を併設しているのでレッスン後やラウンド後の身体のケアもおこなえます。
+      またラウンドの前日に施術を受けることで、ゴルフに使用する背中や肩甲骨・股関節付近の筋肉をほぐし、リラックスした状態でゴルフに望むことができます。
+    </p>
+  </div>
+</div>

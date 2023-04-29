04c356a7b3f7ae6ed036a21d0edfca396d99040b
Moriyama Nagi <naginani072@gmail.com>
2023-04-29 15:38:04 +0900

    [レスポンシブ対応] トップ画像 (#66)
    <!-- #の直後にIssue番号 -->
    Close #65
    
    ## 概要
    
    ## やったこと
    
    ## やらなかったこと
    
    ## 備考
    <!-- 参考にした資料や記事あれば -->

--- src/components/features/top/header/header.php
+++ src/components/features/top/header/header.php
@@ -3,9 +3,9 @@
 ?>
 
 <div class="relative h-fit w-full">
-  <img src='<?php echo $absoluteUriPath ?>/asset/top/top.png' class="object-cover">
+  <img src='<?php echo $absoluteUriPath ?>/asset/top/top.png' class="phone:h-60 phone:object-cover phone:object-left object-contain">
 
   <div class="absolute top-0 flex justify-center items-center h-full w-full overflow-hidden">
-    <img src="<?php echo $absoluteUriPath ?>/asset/top/title.png" />
+    <img src="<?php echo $absoluteUriPath ?>/asset/top/title.png" class="h-3/5 phone:h-4/5 phone:px-4 phone:object-contain object-cover" />
   </div>
 </div>

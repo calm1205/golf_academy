87d36a3faa504d708fc09626d9233480427cbe39
Moriyama Nagi <naginani072@gmail.com>
2023-04-29 12:20:45 +0900

    [施設概要] 練習設備・トラックマン (#58)
    <!-- #の直後にIssue番号 -->
    Close #50 
    
    ## 概要
    
    ## やったこと
    
    ## やらなかったこと
    
    ## 備考
    <!-- 参考にした資料や記事あれば -->

--- src/components/facility/facility.php
+++ src/components/facility/facility.php
@@ -1,4 +1,10 @@
 <img class="h-52 w-full object-cover" src="<?php echo $absoluteUriPath ?>/asset/facility/facility.png" />
 
 
-<h1>facility</h1>
+<?php
+$title = 'トラックマンを使った最新鋭の練習設備';
+ob_start();
+include 'trackman/trackman.php';
+$children = ob_get_clean();
+include $uiPartsPath . '/section/section.php'
+?>

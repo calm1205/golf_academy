ab20613ffc2479b884238365eb2d96dcd7f4da5a
calm1205 <naginani072@gmail.com>
2023-04-29 12:58:26 +0900

    title コメントアウト修正

--- src/components/ui_parts/title/title.php
+++ src/components/ui_parts/title/title.php
@@ -1,8 +1,8 @@
-<!-- ページ内の青グラデーション背景のタイトル -->
-
-<!-- 
-  title: string
- -->
+<?php /*
+  概要: ページ内の青グラデーション背景のタイトル
+  引数:
+    - title: string
+*/ ?>
 
 <div class="py-3 flex justify-center items-center bg-gradient-to-r from-primary-light from-34% to-primary-default border-b-[3px] border-solid border-primary-dark">
   <?php include __DIR__ . '/line.php' ?>

3f9f02d9f015ebacd2b2b7f9233aefc617f01737
Moriyama Nagi <naginani072@gmail.com>
2023-04-29 12:03:46 +0900

    [施設案内] ヘッダー (#54)
    <!-- #の直後にIssue番号 -->
    Close #50 
    
    ## 概要
    
    ## やったこと
    
    ## やらなかったこと
    
    ## 備考
    <!-- 参考にした資料や記事あれば -->

--- src/components/header/menu.php
+++ src/components/header/menu.php
@@ -1,6 +1,14 @@
 <ul class="flex gap-6 text-base">
-  <li>施設概要</li>
-  <li><a href="/rental">レンタル用品</a></li>
-  <li>スタッフ紹介</li>
-  <li>アクセス</li>
+  <li>
+    <a href="/facility">施設概要</a>
+  </li>
+  <li>
+    <a href="/rental">レンタル用品</a>
+  </li>
+  <li>
+    スタッフ紹介
+  </li>
+  <li>
+    アクセス
+  </li>
 </ul>
--- src/components/rental/header/header.php
+++ src/components/rental/header/header.php
@@ -1,4 +1,4 @@
-<img class="h-52 w-full object-cover"  src="<?php echo $absoluteUriPath ?>/asset/rental/shoes.svg" />
+<img class="h-52 w-full object-cover" src="<?php echo $absoluteUriPath ?>/asset/rental/shoes.svg" />
 
 <p class="mt-4 text-center text-xl tracking-wide">
   ゴルフレッスンを手ぶらでも受けられるように、無料のレンタルクラブをご用意しております。<br>ゴルフクラブをお持ちでない方、身軽にレッスンを受講したい方はご利用ください。

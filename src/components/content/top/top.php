bbf20decac23b247ec68eb8586a7722e5c5d0ad2
satohiro213 <satohirosnorlax0213@gmail.com>
2023-04-30 02:28:17 +0900

    "top.phpの画像配置のスタイル調整"

--- src/components/content/top/top.php
+++ src/components/content/top/top.php
@@ -1,8 +1,8 @@
 <!-- コンテンツ ヘッダー -->
 <div class="relative h-fit w-full">
-  <img src='<?php echo $absoluteUriPath ?>/asset/top/top.png' alt="" class="w-full object-cover">
+  <img src='<?php echo $absoluteUriPath ?>/asset/top/top.png' alt="" class="w-full object-cover phone:h-[360px] phone:object-left">
 
-  <div class="absolute top-0 flex justify-center items-center h-full w-full">
+  <div class="absolute top-0 flex justify-center items-center h-full w-full phone:px-2">
     <img src="<?php echo $absoluteUriPath ?>/asset/top/title.png" />
   </div>
 </div>

5752ab627374beaf66d0ffcbee3e935017bc6315
satohiro213 <satohirosnorlax0213@gmail.com>
2023-04-30 05:12:16 +0900

    ”ニュースの矢印がうまく読み込めないので、fontのLinkをcssの前後で２回読み込み解決”

--- src/index.php
+++ src/index.php
@@ -18,6 +18,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <link rel="stylesheet" href="<?php echo $absoluteUriPath ?>/style.css" />
   <link rel="stylesheet" href="<?php echo $absoluteUriPath ?>/tailwind.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
 </head>
 

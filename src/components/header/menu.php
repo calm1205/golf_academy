32cf2b335c4c1267ea0a706e8907022f69e83522
Moriyama Nagi <naginani072@gmail.com>
2023-04-29 12:19:19 +0900

    Revert "[施設概要] 練習設備トラックマン (#56)"
    This reverts commit 8ef4df9234cf32858d8fbee3d95551cdef8fea2c.

--- src/components/header/menu.php
+++ src/components/header/menu.php
@@ -1,14 +1,6 @@
 <ul class="flex gap-6 text-base">
-  <li>
-    <a href="/facility">施設概要</a>
-  </li>
-  <li>
-    <a href="/rental">レンタル用品</a>
-  </li>
-  <li>
-    スタッフ紹介
-  </li>
-  <li>
-    アクセス
-  </li>
+  <li>施設概要</li>
+  <li><a href="/rental">レンタル用品</a></li>
+  <li>スタッフ紹介</li>
+  <li>アクセス</li>
 </ul>

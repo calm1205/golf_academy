b1dfdfd068b9307064f5be55735bd79c24fcfba2
calm1205 <naginani072@gmail.com>
2023-04-29 15:03:15 +0900

    balloonレスポンシブ対応

--- src/components/ui_parts/header/menu.phone.php
+++ src/components/ui_parts/header/menu.phone.php
@@ -1,3 +1,7 @@
+<?php
+// スマホ用メニュー
+?>
+
 <div class="flex flex-col justify-evenly px-1 w-10 h-10 border border-primary-light rounded cursor-pointer">
   <?php for ($i = 0; $i < 3; $i++) { ?>
     <hr class=" border-primary-light" />

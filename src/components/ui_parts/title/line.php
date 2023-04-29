84d824692d4668f20f27aacb5e146aa7bf515045
satohiro213 <satohirosnorlax0213@gmail.com>
2023-04-30 03:23:12 +0900

    "section/titleとlineのスタイル調整"

--- src/components/ui_parts/title/line.php
+++ src/components/ui_parts/title/line.php
@@ -6,5 +6,5 @@
 <?php $line_length = round(sqrt(2 * ($width * $width))); ?>
 
 <div class="h-10" style="width: <?php echo $width; ?>px">
-  <div class="h-10 border-b-[3px] border-solid border-white -rotate-45 origin-bottom-left" style="width: <?php echo $line_length; ?>px"></div>
+  <div class="h-10 border-b-[3px] border-solid border-white -rotate-45 origin-bottom-left phone:hidden" style="width: <?php echo $line_length; ?>px"></div>
 </div>

3e6250f75c675fdb8147864708bb1ecc85f94647
calm1205 <naginani072@gmail.com>
2023-04-30 10:10:37 +0900

    footerレスポンシブ

---
+++ src/components/ui_parts/footer/footer.phone.php
@@ -0,0 +1,21 @@
+<div class="w-full bg-primary-exLight">
+
+  <div class="p-6 flex flex-col gap-3">
+    <p class="text-base">ライジングレディース心斎橋ゴルフスタジオ</p>
+    <p class="text-sm">〒542-0086<br>大阪府大阪市中央区西心斎橋1-10-15<br>ヴィアイン心斎橋 1F</p>
+    <div class="flex mt-2 gap-1">
+      <i class="fa-solid fa-phone"></i>
+      <p class="text-sm">06-6786-8620</p>
+    </div>
+  </div>
+
+  <ol class="[&_li]:border-t [&_li]:border-gray-400 [&_li]:py-4 [&_li]:pl-4">
+    <li> <a href="#">施設概要</a> </li>
+    <li> <a href="#">レンタル用品</a> </li>
+    <li> <a href="#">スタッフ紹介</a> </li>
+    <li> <a href="#">アクセス</a> </li>
+    <li> <a href="#">特定商取引法に基づく表記</a> </li>
+    <li> <a href="#">プライバシーポリシー</a> </li>
+    <li> <a href="#">会員規約</a> </li>
+  </ol>
+</div>

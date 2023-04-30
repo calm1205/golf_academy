c4d8290540541ddedb6da6bd37a2c66fe783cbb3
Moriyama Nagi <naginani072@gmail.com>
2023-04-30 10:14:47 +0900

    [レスポンシブ対応] footer (#73)
    <!-- #の直後にIssue番号 -->
    Close #74 
    
    ## 概要
    
    ## やったこと
    
    ## やらなかったこと
    
    ## 備考
    <!-- 参考にした資料や記事あれば -->

--- src/components/ui_parts/footer/footer.php
+++ src/components/ui_parts/footer/footer.php
@@ -1,42 +1,17 @@
 <footer>
-  <div class="h-56 w-full bg-primary-exLight text-xs py-8 flex items-start justify-center gap-10">
 
-    <div class="flex flex-col gap-2">
-      <p><a href="#">HOME</a></p>
-      <p><a href="#">初回診断</a></p>
-      <p><a href="#">料金</a></p>
-      <p><a href="#">レンタル(無料)</a></p>
-      <p><a href="#">コーチ & 所属選手</a></p>
-      <p><a href="#">感染対策</a></p>
-    </div>
+  <?php
+  // pc
+  ?>
+  <div class="phone:hidden block">
+    <?php include 'footer.pc.php' ?>
+  </div>
 
-    <div class="flex flex-col gap-2">
-      <p><a href="#">初回診断 予約申込</a></p>
-      <p><a href="#">Information</a></p>
-      <p><a href="#">Access</a></p>
-      <p> <a href="#">Contact</a></p>
-    </div>
-
-    <div class="flex flex-col gap-2">
-      <p><a href="#">特定商取引に基づく表記</a></p>
-      <p><a href="#">プライバシーポリシー</a></p>
-      <p><a href="#">会員規約</a></p>
-    </div>
-
-    <div class="flex gap-2">
-      <a href=""><i class="fa-brands fa-instagram"></i></a>
-      <a href=""><i class="fa-brands fa-youtube"></i></a>
-    </div>
-
-
-    <div class="flex flex-col gap-2">
-      <p>ライジングレディース心斎橋ゴルフスタジオ</p>
-      <p>〒542-0086<br>大阪府大阪市中央区西心斎橋1-10-15<br>ヴィアイン心斎橋 1F</p>
-      <div class="flex mt-2 gap-1">
-        <i class="fa-solid fa-phone"></i>
-        <p>06-6786-8620</p>
-      </div>
-    </div>
+  <?php
+  // phone
+  ?>
+  <div class="phone:block hidden">
+    <?php include 'footer.phone.php' ?>
   </div>
 
   <?php include 'copyright.php'; ?>

00d72b4c94a38badbd0793918c3a918988848959
satohiro213 <satohirosnorlax0213@gmail.com>
2023-05-06 08:22:57 +0900

    "Table2の作成"

---
+++ src/components/ui_parts/guidance/guidance2.php
@@ -0,0 +1,11 @@
+<!-- desktop -->
+<div class="flex phone:hidden justify-center gap-5">
+  <img src="<?php echo $absoluteUriPath ?>/asset/guidance/guidance.png" />
+  <?php include 'guidanceTable2.php' ?>
+</div>
+
+<!-- phone -->
+<div class="hidden phone:flex flex-col justify-center gap-5">
+  <img src="<?php echo $absoluteUriPath ?>/asset/guidance/guidance.png" />
+  <?php include 'guidanceTable2.php' ?>
+</div>

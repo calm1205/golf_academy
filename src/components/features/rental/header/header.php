b539ab1070786edfe61077d4ba4bc04b0c82f776
satohiro213 <satohirosnorlax0213@gmail.com>
2023-05-06 07:35:22 +0900

    "headerレイアウト調整"

--- src/components/features/rental/header/header.php
+++ src/components/features/rental/header/header.php
@@ -1,5 +1,5 @@
 <img class="h-52 w-full object-cover" src="<?php echo $absoluteUriPath ?>/asset/rental/shoes.svg" />
 
-<p class="mt-4 text-center text-xl tracking-wide">
+<p class="mt-4 mb-16 text-center text-xl tracking-wide ">
   ゴルフレッスンを手ぶらでも受けられるように、無料のレンタルクラブをご用意しております。<br>ゴルフクラブをお持ちでない方、身軽にレッスンを受講したい方はご利用ください。
 </p>

9fdd543b40228a11669bf8aac59f94974dc9ddb5
satohiro213 <satohirosnorlax0213@gmail.com>
2023-05-06 06:20:07 +0900

    "newsの最大表示件数を調整"

--- src/components/features/top/news/news.php
+++ src/components/features/top/news/news.php
@@ -1,8 +1,8 @@
 <div class="flex justify-center mt-10">
-  <section class="w-4/5 max-h-80 mt-2 flex flex-col overflow-y-scroll">
-    <?php for ($i = 0; $i <= 10; $i++) { ?>
-      <a href="#" class="flex justify-between items-center border-b p-3">
-        <article class="flex gap-6 w-full">
+  <section class="w-4/5 max-h-80 flex flex-col overflow-y-scroll gap-5 ">
+    <?php for ($i = 0; $i < 10; $i++) { ?>
+      <a href="#" class="flex justify-between items-center border-b p-3 ">
+        <article class="flex gap-8 w-full">
           <span class="text-[8px] text-white bg-sky-600 p-1">
             2022/10/10
           </span>

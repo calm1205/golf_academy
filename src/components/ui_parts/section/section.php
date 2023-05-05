c82b374a6c1ac78411c0596c2c41c5f40a88e4bd
satohiro213 <satohirosnorlax0213@gmail.com>
2023-05-06 06:06:00 +0900

    "section.phpのwidth調整、phoney用のdivをそれぞれ作成"

--- src/components/ui_parts/section/section.php
+++ src/components/ui_parts/section/section.php
@@ -11,7 +11,7 @@
     <?php include $uiPartsPath . '/title/title.php'; ?>
   </h2>
 
-  <div class="w-4/5 max-w-6xl mx-auto m-10 py-10 phone:py-4">
+  <div class="w-4/5 phone:w-[calc(100%-32px)] max-w-6xl mx-auto m-10 py-10 phone:py-4">
     <?php echo $children ?>
   </div>
 </section>

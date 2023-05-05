d601cc3b7b5df913a1839e2d4245df3b86343885
satohiro213 <satohirosnorlax0213@gmail.com>
2023-05-06 07:41:19 +0900

    "Sectionのレイアウト調整、写真幅の調整"

--- src/components/features/rental/club/club.php
+++ src/components/features/rental/club/club.php
@@ -1,5 +1,17 @@
-<div class="flex justify-start items-center gap-10">
+<div class="flex phone:hidden justify-start items-center gap-10">
   <img src="<?php echo $absoluteUriPath ?>/asset/rental/club.png" />
+
+  <div class="flex flex-col">
+    <p class="text-base whitespace-pre-line">
+      ゴルフクラブはドライバーからパターまで、すべての種類をご用意。
+      レッスン内容に応じたゴルフクラブをレンタル可能です。
+    </p>
+  </div>
+</div>
+
+<!-- phone -->
+<div class="hidden phone:flex flex-col justify-start items-center gap-10">
+  <img src="<?php echo $absoluteUriPath ?>/asset/rental/club.png" class="w-[calc(100%-32px)]" />
 
   <div class="flex flex-col">
     <p class="text-base whitespace-pre-line">
--- src/components/features/rental/shoes/shoes.php
+++ src/components/features/rental/shoes/shoes.php
@@ -1,5 +1,19 @@
-<div class="flex justify-start items-center gap-10">
+<!-- desktop -->
+<div class="flex phone:hidden justify-start items-center gap-10">
   <img src="<?php echo $absoluteUriPath ?>/asset/rental/shoes.svg" />
+
+  <div class="flex flex-col">
+    <p class="text-base whitespace-pre-line">
+      レンタルシューズも各サイズご用意しております。
+      サンダルやブーツで来場いただいても、シューズを履き替えて
+      レッスンを受講いただけます。
+    </p>
+  </div>
+</div>
+
+<!-- phone -->
+<div class="hidden phone:flex flex-col justify-start items-center gap-10">
+  <img src="<?php echo $absoluteUriPath ?>/asset/rental/shoes.svg" class="w-[calc(100%-32px)]" />
 
   <div class="flex flex-col">
     <p class="text-base whitespace-pre-line">

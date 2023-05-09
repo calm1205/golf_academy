c579f769e5c2cac685329f142e42fb6e15ec0ec5
satohiro213 <satohirosnorlax0213@gmail.com>
2023-05-09 13:16:55 +0900

    "コード簡略化"

--- src/components/features/rental/club/club.php
+++ src/components/features/rental/club/club.php
@@ -1,5 +1,5 @@
-<div class="flex phone:hidden justify-start items-center gap-10">
-  <img src="<?php echo $absoluteUriPath ?>/asset/rental/club.png" />
+<div class="flex phone:flex-col justify-start items-center gap-10">
+  <img src="<?php echo $absoluteUriPath ?>/asset/rental/club.png" class="phone:w-[calc(100%-32px)]" />
 
   <div class="flex flex-col">
     <p class="text-base whitespace-pre-line">
@@ -9,16 +9,5 @@
   </div>
 </div>
 
-<!-- phone -->
-<div class="hidden phone:flex flex-col justify-start items-center gap-10">
-  <img src="<?php echo $absoluteUriPath ?>/asset/rental/club.png" class="w-[calc(100%-32px)]" />
-
-  <div class="flex flex-col">
-    <p class="text-base whitespace-pre-line">
-      ゴルフクラブはドライバーからパターまで、すべての種類をご用意。
-      レッスン内容に応じたゴルフクラブをレンタル可能です。
-    </p>
-  </div>
-</div>
 
 <?php include 'clubTable.php' ?>
--- src/components/features/rental/glove/glove.php
+++ src/components/features/rental/glove/glove.php
@@ -1,18 +1,5 @@
 <!-- desktop -->
-<div class="flex phone:hidden justify-start items-center gap-10">
-  <img src="<?php echo $absoluteUriPath ?>/asset/rental/glove.png" />
-
-  <div class="flex flex-col">
-    <p class="text-base whitespace-pre-line">
-      レンタルグローブは男性用のほかレディースサイズとジュニアサイズもご用意。
-      グローブを忘れてしまった、レッスン中に破れてしまったなどのトラブルがあった際は
-      ご利用ください。
-    </p>
-  </div>
-</div>
-
-<!-- phone -->
-<div class="hidden phone:flex flex-col justify-start items-center gap-10">
+<div class="flex phone:flex-col justify-start items-center gap-10">
   <img src="<?php echo $absoluteUriPath ?>/asset/rental/glove.png" class="w-[calc(100%-32px)]" />
 
   <div class="flex flex-col">

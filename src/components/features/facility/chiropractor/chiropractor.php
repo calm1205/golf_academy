cf6229f5ef5cdb87ea5da44f2eb4aee6db9d65d9
satohiro213 <satohirosnorlax0213@gmail.com>
2023-05-09 13:06:34 +0900

    "コード簡略化"

--- src/components/features/facility/chiropractor/chiropractor.php
+++ src/components/features/facility/chiropractor/chiropractor.php
@@ -1,18 +1,6 @@
 <!-- desktop -->
-<div class="flex phone:hidden justify-start items-center gap-10">
-  <img src="<?php echo $absoluteUriPath ?>/asset/chiropractor/chiropractor.png" />
-
-  <div class="flex flex-col">
-    <p class="text-base whitespace-pre-line">
-      After Golf Studioは整体を併設しているのでレッスン後やラウンド後の身体のケアもおこなえます。
-      またラウンドの前日に施術を受けることで、ゴルフに使用する背中や肩甲骨・股関節付近の筋肉をほぐし、リラックスした状態でゴルフに望むことができます。
-    </p>
-  </div>
-</div>
-
-<!-- phone -->
-<div class="hidden phone:flex flex-col justify-start items-center gap-10">
-  <img src="<?php echo $absoluteUriPath ?>/asset/chiropractor/chiropractor.png" class="w-[calc(100%-32px)]" />
+<div class="flex phone:flex-col justify-start items-center gap-10">
+  <img src="<?php echo $absoluteUriPath ?>/asset/chiropractor/chiropractor.png" class="phone:w-[calc(100%-32px)]" />
 
   <div class="flex flex-col">
     <p class="text-base whitespace-pre-line">

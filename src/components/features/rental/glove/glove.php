24fe42651fb84231c33340d4b11e52114816e951
calm1205 <naginani072@gmail.com>
2023-05-13 19:41:46 +0900

    [rm] comment

--- src/components/features/rental/glove/glove.php
+++ src/components/features/rental/glove/glove.php
@@ -1,4 +1,3 @@
-<!-- desktop -->
 <div class="flex phone:flex-col justify-start items-center gap-10">
   <img src="<?php echo $absoluteUriPath ?>/asset/rental/glove.png" class="w-[calc(100%-32px)]" />
 

044b07025c0040f0346b2431dd46d358642b3120
calm1205 <naginani072@gmail.com>
2023-05-13 19:29:27 +0900

    [feat] guidanceの共通化

--- src/components/ui_parts/guidance/guidance.php
+++ src/components/ui_parts/guidance/guidance.php
@@ -1,11 +1,4 @@
-<!-- desktop -->
-<div class="flex phone:hidden justify-center gap-5">
-  <img src="<?php echo $absoluteUriPath ?>/asset/guidance/guidance.png" />
-  <?php include 'guidanceTable.php' ?>
-</div>
-
-<!-- phone -->
-<div class="hidden phone:flex flex-col justify-center gap-5">
+<div class="flex phone:flex-col justify-center gap-5">
   <img src="<?php echo $absoluteUriPath ?>/asset/guidance/guidance.png" />
   <?php include 'guidanceTable.php' ?>
 </div>

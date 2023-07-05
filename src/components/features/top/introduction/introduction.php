74ebd342454c8e809e75f842712b94d4e1d21c0a
satohiro213 <satohirosnorlax0213@gmail.com>
2023-07-06 01:20:17 +0900

    "セクションパーツ(仮)"

--- src/components/features/top/introduction/introduction.php
+++ src/components/features/top/introduction/introduction.php
@@ -8,11 +8,22 @@ $lesson = 'After Golf Studioではツアーの最前線で活躍するプロゴ�
 <?php
 // desktop 
 ?>
-<div class="pt-16 pb-[100px] flex phone:hidden flex-col gap-10 items-center">
-  <div class="">
-    <img src="<?php echo $absoluteUriPath ?>/asset/introduction/trackman.png">
-    <img src="<?php echo $absoluteUriPath ?>/asset/introduction/newTrackman.png">
+<div class="pt-16 pb-[100px] flex phone:hidden flex-col gap-10 items-center relative">
+  <div class="h-[510px]">
+    <img src="<?php echo $absoluteUriPath ?>/asset/introduction/trackman.png" width="600" height="360" class="absolute top-[42px] left-0" />
+    <img src="<?php echo $absoluteUriPath ?>/asset/introduction/newTrackman.png" width="910" height="510" class="absolute top-0 right-0 -skew-x-12 z-10" />
+    <div class=" w-[800px] flex flex-col pt-10 pr-[24px] pb-10 items-start gap-4 bg-white shadow absolute top-[240px] right-0 z-50">
+      <h2 class="pl-[24px] text-blue-400">
+        トラックマンを使用した最新鋭の練習設備
+      </h2>
+      <hr class="w-full border-b border-solid border-primary-default self-stretch">
+      <p class="flex flex-col pl-[24px] gap-10 self-stretch">
+        <?php echo $trackman ?>
+      </p>
+    </div>
   </div>
+
+
 
   <div class="">
 

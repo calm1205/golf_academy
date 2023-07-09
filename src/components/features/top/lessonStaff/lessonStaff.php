a4b68ee6e0508bf5b5aada02db63161713423cc8
calm1205 <naginani072@gmail.com>
2023-07-09 19:54:07 +0900

    変数の使いまわし

--- src/components/features/top/lessonStaff/lessonStaff.php
+++ src/components/features/top/lessonStaff/lessonStaff.php
@@ -6,20 +6,19 @@ $description = "After Golf Studioのメインコーチを務めるタイガー�
 という方にお伝えしたいと思っております。
 ゴルフの魅力を広め、ゴルフを愛する人をひとりでも増やしていけるように尽力いたします。";
 $image = $absoluteUriPath . '/asset/lesson/staff.png';
-include $uiPartsPath . '/card/card.php';
+include $uiPartsPath . '/card/cardWithButton.php';
 ?>
 
 <!-- phone -->
 <div class="hidden phone:flex flex-col justify-start items-center gap-10 ">
-  <img src="<?php echo $absoluteUriPath ?>/asset/lesson/staff.png" class="w-full" />
+  <img src="<?php echo $image ?>" class="w-full" />
 
   <div class="flex flex-col">
-    <h3 class="text-2xl">代表コーチ<br> タイガー・ウッズプロ</h3>
+    <h3 class="text-2xl">
+      <?php echo $title ?>
+    </h3>
     <p class="text-base whitespace-pre-line">
-      After Golf Studioのメインコーチを務めるタイガー・ウッズです。
-      私がこれまでのゴルフ人生で得た知識・経験を、これからゴルフを上達したい！
-      という方にお伝えしたいと思っております。
-      ゴルフの魅力を広め、ゴルフを愛する人をひとりでも増やしていけるように尽力いたします。
+      <?php echo $description ?>
     </p>
   </div>
 </div>

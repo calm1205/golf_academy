7ec8c4a7e092a817004e2ec12d3bff0135351ffb
calm1205 <naginani072@gmail.com>
2023-07-10 08:55:42 +0900

    [mv] title

--- src/components/ui_parts/section/section.php
+++ src/components/ui_parts/section/section.php
@@ -8,7 +8,7 @@
 
 <section>
   <h2>
-    <?php include $uiPartsPath . '/title/title.php'; ?>
+    <?php include 'title/title.php'; ?>
   </h2>
 
   <div class="w-4/5 phone:w-[calc(100%-32px)] max-w-6xl mx-auto m-10 py-10 phone:py-4">

86bdda1cd30b686945d23035f76b1bfb2eb485c9
satohiro213 <satohirosnorlax0213@gmail.com>
2023-07-10 01:22:19 +0900

    "section内部logo画像の配置"

--- src/components/ui_parts/title/title.php
+++ src/components/ui_parts/title/title.php
@@ -4,10 +4,9 @@
     - title: string
 */ ?>
 
-<div class="py-3 flex justify-center items-center bg-gradient-to-r from-primary-light from-34% to-primary-default border-b-[3px] border-solid border-primary-dark">
-  <?php include 'line.php' ?>
+<div class="flex justify-center items-center bg-gradient-to-r from-primary-light from-34% to-primary-default border-b-[3px] border-solid border-primary-dark">
+  <img src="<?php echo $absoluteUriPath ?>/asset/title/logo.svg" alt="" class="h-[128px] mr-[32px]">
   <div class="inline-block phone:text-2xl text-[40px] text-white font-bold text-center">
     <?php echo $title ?>
   </div>
-  <?php include 'line.php' ?>
 </div>

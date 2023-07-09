5c05033b2905b31d4f895cc01a164331aa2bb7c3
satohiro213 <satohirosnorlax0213@gmail.com>
2023-07-09 22:37:31 +0900

    ”Wrpper、Section構成、Table実装”

---
+++ src/components/features/prices/prices.php
@@ -0,0 +1,113 @@
+<div class="w-4/5 flex flex-col gap-20 items-center pt-10 pb-[100px]
+[&_section]:flex [&_section]:flex-col [&_section]:gap-4
+[&_h2]:font-bold [&_h2]:text-2xl [&_h2]:text-center
+[&_hr]:
+<!-- Tableのレイアウト -->
+[&_th]:border [&_th]:inline-block [&_td]:border [&_td]:inline-block
+[&_th]:w-[210px] [&_td]:w-[210px]
+[&_th]:px-10 [&_th]:py-4 [&_td]:px-10 [&_td]:py-4">
+
+  <section>
+    <h2>入会金・事務手数料</h2>
+    <table>
+      <tr>
+        <th>入会金</th>
+        <td>11,000円(税込)</td>
+      </tr>
+      <tr>
+        <th>事務手数料</th>
+        <td>3,000円(税込)</td>
+      </tr>
+    </table>
+
+  </section>
+
+  <section>
+    <h2>気軽に始める基本コース</h2>
+    <tr>
+      <th>受講料金</th>
+      <td>15,000円</td>
+    </tr>
+    <tr>
+      <th>受講時間</th>
+      <td>1回50分 月2回</td>
+    </tr>
+  </section>
+
+  <section>
+    <h2>しっかり上達コース</h2>
+
+    <table>
+      <tr>
+        <th>受講料金</th>
+        <td>25,000円</td>
+      </tr>
+      <tr>
+        <th>受講時間</th>
+        <td>1回50分 月4回</td>
+      </tr>
+    </table>
+
+  </section>
+
+  <section>
+    <h2>ジュニアコース(高校生以下)</h2>
+    <table>
+      <tr>
+        <th>受講料金</th>
+        <td>20,000円</td>
+      </tr>
+      <tr>
+        <th>受講時間</th>
+        <td>1回50分 月4回</td>
+      </tr>
+    </table>
+  </section>
+
+  <section>
+    <h2>親子レッスン</h2>
+    <table>
+      <tr>
+        <th>受講料金</th>
+        <td>35,000円</td>
+      </tr>
+      <tr>
+        <th>受講時間</th>
+        <td>1回50分 月4回</td>
+      </tr>
+    </table>
+
+  </section>
+
+  <section>
+    <h2>オプションコース</h2>
+    <table>
+      <tr>
+        <th>レッスン延長</th>
+        <td>1時間5,000円</td>
+      </tr>
+      <tr>
+        <th>追加単発レッスン</th>
+        <td>8,000円</td>
+      </tr>
+      <tr>
+        <th>ラウンドレッスン</th>
+        <td>都度ご案内</td>
+      </tr>
+    </table>
+
+  </section>
+
+  <section>
+    <h2>ゴルフ整体</h2>
+    <table>
+      <tr>
+        <th>施術料金</th>
+        <td>1回6,000円</td>
+      </tr>
+    </table>
+  </section>
+
+
+
+</div>

206c58ca508410b4756ba11468ab9120159a2e8f
Moriyama Nagi <naginani072@gmail.com>
2023-07-09 19:56:17 +0900

    スタッフ紹介のカードをコンポーネント化 (#87)
    <!-- #の直後にIssue番号 -->
    Close #
    
    ## 概要
    
    ## やったこと
    <img width="359" alt="スクリーンショット 2023-07-09 19 55 55"
    src="https://github.com/calm1205/golf_academy/assets/62320673/2b003ba2-f493-4702-bca8-2c8433048833">
    
    
    ## やらなかったこと
    
    ## 備考
    <!-- 参考にした資料や記事あれば -->

--- src/components/features/top/lessonStaff/lessonStaff.php
+++ src/components/features/top/lessonStaff/lessonStaff.php
@@ -1,41 +1,24 @@
 <!-- desktop -->
-<div class="phone:hidden px-10 py-16 drop-shadow-lg bg-white">
-  <div class=" flex justify-start items-center gap-10 ">
-
-    <img src="<?php echo $absoluteUriPath ?>/asset/lesson/staff.png" />
-
-    <div class="flex flex-col">
-      <h3 class="text-2xl">代表コーチ<br> タイガー・ウッズプロ</h3>
-      <hr class="w-full border-b border-solid border-primary-default self-stretch mt-6">
-      <p class="text-base whitespace-pre-line">
-        After Golf Studioのメインコーチを務めるタイガー・ウッズです。
-        私がこれまでのゴルフ人生で得た知識・経験を、これからゴルフを上達したい！
-        という方にお伝えしたいと思っております。
-        ゴルフの魅力を広め、ゴルフを愛する人をひとりでも増やしていけるように尽力いたします。
-      </p>
-    </div>
-
-  </div>
-  <div class="mt-10 text-center">
-    <?php
-    $button = 'スタッフ紹介を見る';
-    include $uiPartsPath . '/button/button.php'
-    ?>
-  </div>
-</div>
+<?php
+$title = "代表コーチ<br>タイガー・ウッズプロ";
+$description = "After Golf Studioのメインコーチを務めるタイガー・ウッズです。
+私がこれまでのゴルフ人生で得た知識・経験を、これからゴルフを上達したい！
+という方にお伝えしたいと思っております。
+ゴルフの魅力を広め、ゴルフを愛する人をひとりでも増やしていけるように尽力いたします。";
+$image = $absoluteUriPath . '/asset/lesson/staff.png';
+include $uiPartsPath . '/card/cardWithButton.php';
+?>
 
 <!-- phone -->
 <div class="hidden phone:flex flex-col justify-start items-center gap-10 ">
-  <img src="<?php echo $absoluteUriPath ?>/asset/lesson/staff.png" class="w-full" />
+  <img src="<?php echo $image ?>" class="w-full" />
 
   <div class="flex flex-col">
-    <h3 class="text-2xl">代表コーチ<br> タイガー・ウッズプロ</h3>
+    <h3 class="text-2xl">
+      <?php echo $title ?>
+    </h3>
     <p class="text-base whitespace-pre-line">
-      After Golf Studioのメインコーチを務めるタイガー・ウッズです。
-      私がこれまでのゴルフ人生で得た知識・経験を、これからゴルフを上達したい！
-      という方にお伝えしたいと思っております。
-      ゴルフの魅力を広め、ゴルフを愛する人をひとりでも増やしていけるように尽力いたします。
+      <?php echo $description ?>
     </p>
   </div>
-
 </div>
---
+++ src/components/ui_parts/card/cardWithButton.php
@@ -0,0 +1,29 @@
+<?php /*
+  概要: ボタン付きカード
+  引数: 
+    - title: タイトル
+    - description: 説明文
+    - image: 画像URL
+    - button: ボタンの表示テキスト
+*/ ?>
+
+<div class="phone:hidden px-10 py-16 drop-shadow-lg bg-white rounded-2xl">
+  <div class="flex justify-start items-center gap-10 ">
+    <img src="<?php echo $image ?>" />
+    <div class="flex flex-col w-full">
+      <h3 class="text-2xl">
+        <?php echo $title ?>
+      </h3>
+
+      <hr class="border-b border-solid border-primary-default self-stretch mt-6">
+
+      <p class="text-base whitespace-pre-line">
+        <?php echo $description ?>
+      </p>
+    </div>
+  </div>
+
+  <div class="mt-10 text-center">
+    <?php include $uiPartsPath . '/button/button.php' ?>
+  </div>
+</div>

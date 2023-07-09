5dd9d614a99d5bf3c9a63f5351a6afba2bf60102
Moriyama Nagi <naginani072@gmail.com>
2023-07-10 08:50:06 +0900

    料金プランのセクションをループ (#92)
    <!-- #の直後にIssue番号 -->
    Close #
    
    ## 概要
    
    ## やったこと
    以下のUIをループ処理で表現
    
    ![スクリーンショット 2023-07-10 8 48
    47](https://github.com/calm1205/golf_academy/assets/62320673/befa0543-36d3-4597-80e7-11996683ff24)
    
    
    ## やらなかったこと
    
    ## 備考
    <!-- 参考にした資料や記事あれば -->

---
+++ src/components/features/prices/notes/notes.php
@@ -0,0 +1,18 @@
+<div class="border-x border-y block">
+  <h2 class="border-b text-center text-3xl font-bold py-6">注意事項</h2>
+
+  <div class="px-10 pt-6 pb-10">
+    <h3 class="text-left font-bold ml-0 mr-auto text-xl">休会・退会について</h3>
+
+    <ul class="mt-4 [&_li]:mb-4 [&_li]:text-lg text-left ml-0 mr-auto">
+      <li>・休会、退会される場合、必ず店頭でのお手続きが必要です。お電話・FAX・郵送による受付は行っておりません。</li>
+      <li>・休会は1ヶ月単位での手続きが出来、最大6ヶ月まで可能です。（休会の延長をご希望の場合は再度届出が必要）</li>
+      <li>・休会・退会の手続き締切日はご利用希望最終月の前月末迄です。（12月末退会希望の場合、11月末迄に手続きが必要）</li>
+    </ul>
+
+    <ul class="[&_li]:mb-2 text-left ml-0 mr-auto text-sm">
+      <li>※休会・退会のお申出が遅くなり、翌月分の引落が行われた場合には返金致しかねますの でご了承ください。 </li>
+      <li>※休会中及び退会後はレッスンの振替及び打ち放題の利用は不可となります</li>
+    </ul>
+  </div>
+</div>
---
+++ src/components/features/prices/section/section.php
@@ -0,0 +1,31 @@
+<?php /*
+  概要: 料金プランのセクション
+  引数: 
+    - title: string
+    - description: string | null
+    - table: string[]
+*/ ?>
+
+<section class="flex flex-col items-center gap-4">
+  <h2 class="font-bold text-2xl"> <?php echo $title ?> </h2>
+
+  <div class="flex flex-col items-center">
+    <hr class="w-[100px] border-b border-solid border-[#333]">
+
+    <?php if ($description) { ?>
+      <p class="text-center whitespace-pre"> <?php echo $description ?> </p>
+    <?php } ?>
+  </div>
+
+  <table class="border-collapse border 
+  [&_th]:border [&_th]:w-[210px] [&_th]:py-4 [&_th]:font-normal
+  [&_td]:border [&_td]:px-10 [&_td]:py-4
+  ">
+    <?php foreach ($table as $key => $value) { ?>
+      <tr>
+        <th><?php echo $key ?></th>
+        <td><?php echo $value[0] ?></td>
+      </tr>
+    <?php }  ?>
+  </table>
+</section>

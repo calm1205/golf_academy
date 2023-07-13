0a242141ebbaf6a10ed2d8cad27df3d58b5a21be
satohiro213 <satohirosnorlax0213@gmail.com>
2023-07-13 09:54:34 +0900

    "cardパーツのdescriptionがパーツからはみ出していたのを修正"

--- src/components/ui_parts/card/card.php
+++ src/components/ui_parts/card/card.php
@@ -11,7 +11,7 @@
   </h2>
 
   <hr class="w-full border-b border-solid border-primary-default">
-  <p class="pl-6 gap-10 whitespace-pre"> <?php echo $description ?> </p>
+  <p class="pl-6 gap-10"> <?php echo $description ?> </p>
 
   <?php if ($button) { ?>
     <div class="ml-[24px] mt-2">

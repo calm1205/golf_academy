e23119b10d626e143ca27d706b566b9e56493471
satohiro213 <satohirosnorlax0213@gmail.com>
2023-07-17 02:32:34 +0900

    "施設概要ページのレイアウト"

--- src/components/features/facility/facility.php
+++ src/components/features/facility/facility.php
@@ -1,6 +1,6 @@
 <img class="h-52 w-full object-cover phone:mb-6" src="<?php echo $absoluteUriPath ?>/asset/facility/facility.png" />
 
-<div class="flex flex-col gap-12 mt-14 w-4/5 mx-auto">
+<div class="flex flex-col gap-12 mt-14 px-4 ">
   <?php
   // トラックマンを使用した最新鋭の練習設備
   include 'trackman/trackman.php';
--- src/components/features/facility/trackman/trackman.php
+++ src/components/features/facility/trackman/trackman.php
@@ -28,14 +28,19 @@ $data = 'トラックマンでは弾道や飛距離だけでなく、クラブ�
 
 <!-- phone -->
 <div class="hidden phone:flex flex-col gap-8">
-  <img src="<?php echo $absoluteUriPath ?>/asset/facility/trackman/trackman1.png" />
-  <p class="ml-4 text-base">
-    <?php echo $trackman2 ?>
-  </p>
-  <hr class="w-full border-b border-solid border-primary-default" />
+  <?php
+  $title = "トラックマンを使用した最新鋭の練習設備";
+  $description = "トッププロも使用する最先端の弾道測定器「トラックマン」を設置。
+  仕切りを設けてプライベート空間を維持した練習場を2打席用意しております。";
+  $image = $absoluteUriPath . '/asset/introduction/trackman.png';
+  $button = null;
+  include $uiPartsPath . '/card/phoneCard.php'; ?>
 
-  <img src="<?php echo $absoluteUriPath ?>/asset/facility/trackman/trackman2.png" />
-  <p class="text-base">
-    <?php echo $data ?>
-  </p>
+  <?php
+  $title = "データをもとに理想のスイングを追求";
+  $description = "トラックマンでは弾道や飛距離だけでなく、クラブの軌道やスイングスピードも計測可能です。
+  取得したあらゆるデータをレッスンに使用することで、理想のスイングを追求します。";
+  $image = $absoluteUriPath . '/asset/facility/trackman/trackman2.png';
+  $button = null;
+  include $uiPartsPath . '/card/phoneCard.php'; ?>
 </div>

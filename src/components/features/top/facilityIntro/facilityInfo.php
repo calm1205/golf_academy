8a9d6d0da57f0d391a950e8ce67e7ffab732db15
satohiro213 <satohirosnorlax0213@gmail.com>
2023-07-17 19:55:17 +0900

    "phone.card.phpに置換"

--- src/components/features/top/facilityIntro/facilityInfo.php
+++ src/components/features/top/facilityIntro/facilityInfo.php
@@ -36,21 +36,21 @@
   $description = "ゴルフクラブなどのレッスンで使用する道具は無料でレンタルをしております。<br>まだ道具を揃えていないビギナーの方でも、安心してレッスンを受けることが可能です。<br>レンタル用品の詳細は、以下のリンクよりご確認ください。";
   $image = $absoluteUriPath . '/asset/handFree/clubs.png';
   $button = null;
-  include $uiPartsPath . '/card/phoneCard.php'; ?>
+  include $uiPartsPath . '/card/phone.card.php'; ?>
 
   <?php
   $title = "ジュニアゴルフレッスンも大募集！";
   $description = "ゴルフの基本動作の習得から、スコアを伸ばすための本格的なゴルフレッスンまで指導可能です。<br>ゴルフを楽しみたいお子さまも、プロを目指してハイレベルのレッスンを受けたいお子さまにも、それぞれに合った個別指導をおこなっています。";
   $image = $absoluteUriPath . '/asset/junior/junior.png';
   $button = null;
-  include $uiPartsPath . '/card/phoneCard.php'; ?>
+  include $uiPartsPath . '/card/phone.card.php'; ?>
 
 
   <?php
   $title = "ゴルファー専門整体施設も併設";
   $description = "骨盤や股関節、肩甲骨を重点的に施術し、ゴルフを正しい動作でプレーするための身体作りをサポートします。<br>ゴルフに使用する様々な筋肉をケアすることで、飛距離の向上や、疲れにくい身体づくりをすることが可能です。";
   $image = $absoluteUriPath . '/asset/chiropractor/chiropractor.png';
-  include $uiPartsPath . '/card/phoneCard.php'; ?>
+  include $uiPartsPath . '/card/phone.card.php'; ?>
 
 
 </div>

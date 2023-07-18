99f86184be7acd6d42aeac9bf343a81074637d77
satohiro213 <127710433+satohiro213@users.noreply.github.com>
2023-07-18 15:39:10 +0900

    Feature/new responsive top (#95)
    <!-- #の直後にIssue番号 -->
    Close #
    
    ## 概要
    Topページのレスポンシブ対応
    
    ## やったこと
    概要の通り
    
    ## やらなかったこと
    スマホでのレイアウト確認
    
    ## 備考
    <!-- 参考にした資料や記事あれば -->

--- src/components/ui_parts/guidance/guidance.php
+++ src/components/ui_parts/guidance/guidance.php
@@ -1,3 +1,12 @@
+<div class="flex flex-col text-center mx-auto w-[144px]">
+  <h2 class="text-2xl font-bold text-center pb-2">
+    営業のご案内
+  </h2>
+  <hr class="w-[100px] border-b border-solid border-[#333] mb-4 text-center mx-auto">
+</div>
+
+
+
 <div class="flex phone:flex-col justify-center gap-5">
   <img src="<?php echo $absoluteUriPath ?>/asset/guidance/guidance.png" />
   <?php include 'guidanceTable.php' ?>

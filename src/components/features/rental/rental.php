d1c3a222cd90142d6eadcf5cad6ad4aaad6bebc3
satohiro213 <satohirosnorlax0213@gmail.com>
2023-07-19 15:22:38 +0900

    "レンタル用品ページのパーツ埋め込みや字寄せなど"

--- src/components/features/rental/rental.php
+++ src/components/features/rental/rental.php
@@ -9,20 +9,19 @@
   <div class="mt-16"></div>
   <?php
   $title = "レンタルシューズ";
-  $description = "レンタルシューズも各サイズご用意しております。
+  $description = "レンタルシューズも各サイズご用意しております。<br>
   サンダルやブーツで来場いただいても、シューズを履き替えてレッスンを受講いただけます。";
-  $image = $absoluteUriPath . "/asset/rental/shoes.svg";
-  include 'card/card.php'
-  ?>
+  $image = $absoluteUriPath . '/asset/rental/shoes.svg';
+  $button = null;
+  include $uiPartsPath . '/card/phone.card.php'; ?>
 
   <div class="mt-16"></div>
   <?php
   $title = "レンタルグローブ";
-  $description = "レンタルグローブは男性用のほかレディースサイズとジュニアサイズもご用意。
-  グローブを忘れてしまった、レッスン中に破れてしまったなどのトラブルがあった際はご利用ください。";
-  $image = $absoluteUriPath . "/asset/rental/glove.png";
-  include 'card/card.php'
-  ?>
+  $description = "レンタルグローブは男性用のほかレディースサイズとジュニアサイズもご用意。<br>グローブを忘れてしまった、レッスン中に破れてしまったなどのトラブルがあった際はご利用ください。";
+  $image = $absoluteUriPath . '/asset/rental/glove.png';
+  $button = null;
+  include $uiPartsPath . '/card/phone.card.php'; ?>
 
 </div>
 

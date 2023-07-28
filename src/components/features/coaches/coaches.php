54c1f952edd84fb3aea419d569458c950d256a91
satohiro213 <satohirosnorlax0213@gmail.com>
2023-07-29 00:04:45 +0900

    "ページ下部仮完成と全体の微修正"

--- src/components/features/coaches/coaches.php
+++ src/components/features/coaches/coaches.php
@@ -4,11 +4,11 @@ $image = $absoluteUriPath . '/asset/prices/prices.png';
 include $uiPartsPath . '/header/headerImage/headerImage.php';
 ?>
 
-<div class="my-20 mx-auto flex flex-col gap-20 w-4/5 ">
+<div class="my-20 mx-auto flex flex-col w-4/5 ">
 
-  <div class="flex gap-6">
+  <div class="flex gap-6 mb-20">
     <?php
-    $image = $absoluteUriPath . '/asset/coachesA/coach.png';
+    $image = $absoluteUriPath . '/asset/coaches/coach1.png';
     ?>
     <img src="<?php echo  $image ?>">
 
@@ -40,8 +40,8 @@ include $uiPartsPath . '/header/headerImage/headerImage.php';
         <p>現在50歳で、昨年よりPGAシニアツアーの出場を目指し、2年連続でQT決勝に進出している。<br>リアルトーナメントプレーヤーであり、自分のベースを作るために新たな挑戦として、インドアゴルフスタジオのヘッドコーチに就任した。<br>埼玉県に戻り、自身の出身地である場所でゴルフの楽しさを皆様に伝えるために頑張ります。</p>
 
       </div>
-      <?php $image =  $absoluteUriPath . '/asset/coachesA/twoSpuare.png'; ?>
-      <img src="<?php echo $image ?>" alt="">
+      <?php $image =  $absoluteUriPath . '/asset/coaches/twoSpuares.png'; ?>
+      <img src="<?php echo $image ?>" class="">
 
 
 
@@ -50,8 +50,43 @@ include $uiPartsPath . '/header/headerImage/headerImage.php';
 
   </div>
 
-  <div class="flex flex-row-reverse gap-6">
+  <div class="flex flex-row-reverse gap-6 mb-10">
+
+    <?php
+    $image = $absoluteUriPath . '/asset/coaches/coach2.png';
+    ?>
+    <img src="<?php echo  $image ?>">
+
+    <div class="flex flex-col gap-4">
+
+      <div>
+        <h3 class="text-xl">Kokubo Yukari</h3>
+        <h2 class="text-3xl font-bold mb-0">小久保由加里</h2>
+      </div>
+
+      <hr class="border-b border-solid border-primary-default self-stretch">
+
+      <div class="flex flex-col gap-2">
+
+        <div class="flex gap-2">
+          <p class="text-white bg-gray-700 rounded text-center w-[136px]">所属</p>
+          <p>一般社団法人 日本女子プロゴルフ協会員（入会：2010年8月1日（82期生））</p>
+        </div>
+
+        <div class="flex gap-2">
+          <p class="text-white bg-gray-700 rounded text-center w-[136px] h-6">レッスンスタイル</p>
+          <p>一人ひとりに合ったアドバイスで体に負担が少なく楽しいゴルフが出来るように、<br>お役に立てるレッスンをしたいと思います。</p>
+        </div>
+
+      </div>
+
+      <p>高校卒業後バスケット女子日本リーグ一部の強豪・日本通運の社会人選手として活躍。6年後に退社し、以前から興味のあったプロゴルファーを目指しゴルフ場の研修生に。41歳史上2番目の年長記録でプロテストに合格。<br>現在レジェンズツアー参戦中。<br>将来を見据え新たな挑戦としてインドアゴルフスタジオのヘッドコーチに就任。<br>夫 小久保晃一プロの地元である埼玉県で二人三脚で皆様にゴルフの楽しさを伝える事を楽しみにしています。</p>
+
+    </div>
 
   </div>
+
+  <?php $image =  $absoluteUriPath . '/asset/coaches/twoSquaresReverse.png'; ?>
+  <img src="<?php echo $image ?>" class="w-[100px] h-[100px]">
 
 </div>

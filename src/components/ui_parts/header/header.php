cae1e00ffe5d8f7556c1cced101b6c284476d4aa
satohiro213 <127710433+satohiro213@users.noreply.github.com>
2023-07-29 20:01:50 +0900

    Feature/images (#103)
    <!-- #の直後にIssue番号 -->
    Close #
    
    ## 概要
    コーチ紹介ページ以外の画像の差し替え
    
    ## やったこと
    概要の通り
    
    ## やらなかったこと
    Headerの背景画像がうまくエクスポートできなくてちゃんと実装できていません。。一旦リクエスト出します、他作業後に修正します。
    
    <img width="1044" alt="スクリーンショット 2023-07-28 20 25 49"
    src="https://github.com/calm1205/golf_academy/assets/127710433/ab3aee91-1bd2-4b28-92e2-c45b0b093088">
    ↑背景画像のみのエクスポートができていません。
    
    ## 備考
    <!-- 参考にした資料や記事あれば -->

--- src/components/ui_parts/header/header.php
+++ src/components/ui_parts/header/header.php
@@ -1,7 +1,7 @@
 <header class="h-24 px-4 flex sticky top-0 w-full items-center justify-between bg-white z-50 gap-6">
 
   <a href="/" class="cursor-pointer flex items-center gap-3">
-    <img class="" src="<?php echo $absoluteUriPath ?>/asset/header/logo.png" />
+    <img class="" src="<?php echo $absoluteUriPath ?>/asset/header/logo.svg" />
   </a>
 
   <nav class="phone:hidden flex w-fit items-end h-24 pb-3">

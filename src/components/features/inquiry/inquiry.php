2706c709e90e7cd2fcbbb5334ed207f30cd68331
Moriyama Nagi <naginani072@gmail.com>
2023-08-06 15:31:05 +0900

    [お問い合わせ] UI微修正 (#111)
    <!-- #の直後にIssue番号 -->
    Close #
    
    ## 概要
    
    ## やったこと
    - 電話アイコンを画像からfont-awesomeに変更
    - 白抜き背景のサイズ調整

--- src/components/features/inquiry/inquiry.php
+++ src/components/features/inquiry/inquiry.php
@@ -2,15 +2,12 @@
   <h1 class="text-blue-400 text-5xl">お問い合わせ</h1>
 </div>
 
-<div class="my-20 phone:my-10 flex flex-col gap-20 phone:gap-6 mx-auto phone:mx-4 bg-white ">
-
-  <!-- denwa -->
+<div class="max-w-5xl py-20 phone:my-10 flex flex-col gap-20 phone:gap-6 mx-auto phone:mx-4 bg-white">
   <div class="flex flex-col gap-[14px] items-center">
     <h3 class="text-xl font-bold">お急ぎの方はお電話にてご連絡ください！</h3>
 
-    <div class="flex gap-2 border border-blue-400 rounded-4 rounded p-[10px]">
-      <?php $image = $absoluteUriPath . '/asset/inquiry/phone.png'; ?>
-      <img src="<?php echo $image ?>">
+    <div class="flex gap-2 border items-center border-blue-400 rounded-4 rounded p-[10px]">
+      <i class="fa-solid fa-phone text-blue-400"></i>
       <a href="tel:0480-53-3334">
         <p class="text-blue-400 font-bold">0480-53-3334</p>
       </a>
@@ -19,9 +16,7 @@
     <p>営業時間：10:00〜21:00 | 定休日：水曜日</p>
   </div>
 
-  <!-- fo-mu -->
   <form class="flex flex-col gap-10 w-[750px] phone:w-full mx-auto ">
-
     <div class="flex flex-col gap-2">
       <p class="font-bold">名前（必須）</p>
       <input type="text" name="NAME" class="border w-full" required>

d58b768edfd22f810573f9348d4f399346fa655d
calm1205 <naginani072@gmail.com>
2023-08-06 12:13:29 +0900

    [fix] style

--- src/components/features/top/news/news.php
+++ src/components/features/top/news/news.php
@@ -1,11 +1,11 @@
 <!-- desktop -->
-<div class="flex phone:hidden justify-center mt-10">
+<div class="flex phone:hidden justify-center mt-10 p-8 bg-white">
   <section class="w-4/5 max-h-80 flex flex-col overflow-y-scroll gap-5 ">
     <?php for ($i = 0; $i < 10; $i++) { ?>
       <a href="#" class="flex justify-between items-center border-b p-3 ">
         <article class="flex gap-8 w-full">
-          <span class="text-[8px] text-white bg-sky-600 p-1">
-            2022/10/10
+          <span class="text-[8px] text-white bg-primary-default p-1 rounded">
+            2022.10.10
           </span>
           <p class="max-w-[80%] text-base text-gray-600 tracking-wider font-extralight text-ellipsis whitespace-nowrap overflow-hidden">
             ニュース仮テキスト〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇
@@ -21,13 +21,13 @@
 </div>
 
 <!-- phone -->
-<div class="hidden phone:flex justify-center mt-10">
+<div class="hidden phone:flex justify-center mt-10 py-8 bg-white">
   <section class="w-[calc(100%-32px)] max-h-80 flex flex-col overflow-y-scroll gap-5 ">
     <?php for ($i = 0; $i < 10; $i++) { ?>
       <a href="#" class="flex justify-between items-center border-b p-3 ">
         <article class="flex gap-8 w-full">
-          <span class="text-[8px] text-white bg-sky-600 p-1">
-            2022/10/10
+          <span class="text-[8px] text-white bg-primary-default p-1 rounded">
+            2022.10.10
           </span>
           <p class="max-w-[80%] text-base text-gray-600 tracking-wider font-extralight text-ellipsis whitespace-nowrap overflow-hidden">
             ニュース仮テキスト〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇〇

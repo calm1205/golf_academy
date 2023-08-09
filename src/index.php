3a7a9a5ac3e664d09ce1d09fff95354efc07a3c1
satohiro213 <satohirosnorlax0213@gmail.com>
2023-08-09 14:46:59 +0900

    "画像取り込み、ファビコンとタイトルの修正"

--- src/index.php
+++ src/index.php
@@ -14,8 +14,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <title>After Golf Studio</title>
-  <link rel="icon" type="image/svg+xml" href='<?php echo $absoluteUriPath ?>/asset/favicon.svg' />
+  <title>ゴルトモアカデミー</title>
+  <link rel="icon" type="image/png" href='<?php echo $absoluteUriPath ?>/asset/favicon.png' />
   <link rel="stylesheet" href="<?php echo $absoluteUriPath ?>/style.css" />
   <link rel="stylesheet" href="<?php echo $absoluteUriPath ?>/tailwind.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">

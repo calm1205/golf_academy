09a4e2798819c1539198b2afe062c3d3822fef34
satohiro213 <satohirosnorlax0213@gmail.com>
2023-08-10 03:01:15 +0900

    "7.Card内テキスト変更"

--- src/components/features/rental/club/club.php
+++ src/components/features/rental/club/club.php
@@ -1,6 +1,8 @@
 <?php
 $title = "レンタルクラブ";
-$description = "ゴルフクラブはドライバーからパターまで、すべての種類をご用意。<br>レッスン内容に応じたゴルフクラブをレンタル可能です。";
+$description = "ゴルフクラブはドライバーからパターまで、すべての種類をご用意。レッスン内容に応じたゴルフクラブをレンタル可能です。<br>
+また、ゴルトモアカデミーでは最新のクラブも事前予約制にてレンタル可能です。もちろんトラックマンでのデータ計測も可能ですので、ぜひご利用ください。
+";
 $image = $absoluteUriPath . '/asset/rental/clubs.jpg';
 $button = null;
 include $uiPartsPath . '/card/card.php'; ?>

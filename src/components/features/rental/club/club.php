7a9fce7fb777bd98a33adb5aa99747e10ef4bd33
satohiro213 <satohirosnorlax0213@gmail.com>
2023-08-10 03:03:39 +0900

    "8.Tableのコメントアウト"

--- src/components/features/rental/club/club.php
+++ src/components/features/rental/club/club.php
@@ -7,9 +7,11 @@ $image = $absoluteUriPath . '/asset/rental/clubs.jpg';
 $button = null;
 include $uiPartsPath . '/card/card.php'; ?>
 
-<h2 class="border-b-4 m-0 p-0 mb-1 border-blue-400 mt-[100px] font-bold text-2xl">
+<!-- <h2 class="border-b-4 m-0 p-0 mb-1 border-blue-400 mt-[100px] font-bold text-2xl">
   セットリスト
-</h2>
+</h2> -->
 
-<?php include 'clubTable.php' ?>
-<?php include 'clubTable.phone.php' ?>
+<?php // include 'clubTable.php' 
+?>
+<?php // include 'clubTable.phone.php' 
+?>

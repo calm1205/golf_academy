867fbf792ec14ab552dba06def79cf550ce79e97
satohiro213 <satohirosnorlax0213@gmail.com>
2023-08-11 01:27:02 +0900

    "Footerの修正"

--- src/components/ui_parts/footer/footer.pc.php
+++ src/components/ui_parts/footer/footer.pc.php
@@ -1,24 +1,16 @@
 <div class="h-56 w-full bg-primary-exLight text-xs py-8 flex items-start justify-center gap-10">
   <div class="flex flex-col gap-2">
     <p><a href="#">HOME</a></p>
-    <p><a href="#">初回診断</a></p>
-    <p><a href="#">料金</a></p>
-    <p><a href="#">レンタル(無料)</a></p>
-    <p><a href="#">コーチ & 所属選手</a></p>
-    <p><a href="#">感染対策</a></p>
+    <p><a href="#">レンタル用品</a></p>
+    <p><a href="#">コーチ紹介</a></p>
+    <p><a href="#">料金プラン</a></p>
   </div>
 
   <div class="flex flex-col gap-2">
-    <p><a href="#">初回診断 予約申込</a></p>
-    <p><a href="#">Information</a></p>
-    <p><a href="#">Access</a></p>
-    <p><a href="#">Contact</a></p>
-  </div>
-
-  <div class="flex flex-col gap-2">
+    <p><a href="#">アクセス</a></p>
     <p><a href="#">特定商取引に基づく表記</a></p>
     <p><a href="#">プライバシーポリシー</a></p>
-    <p><a href="#">会員規約</a></p>
+    <p><a href="#">お問い合わせ</a></p>
   </div>
 
   <div class="flex gap-2">
@@ -28,10 +20,10 @@
 
   <div class="flex flex-col gap-2">
     <p>ゴルトモアカデミー</p>
-    <p>〒347-0009 埼玉県加須市上三俣645ｰ1</p>
+    <p>〒347-0006 埼玉県加須市上三俣645ｰ1</p>
     <div class="flex mt-2 gap-1">
       <i class="fa-solid fa-phone"></i>
-      <p>06-6786-8620</p>
+      <p>06-6786-8620 FAX:0480-53-3324</p>
     </div>
   </div>
 </div>

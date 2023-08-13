a031444d4a4a0b6b0e943f9cc99ebf0262346b69
calm1205 <naginani072@gmail.com>
2023-08-13 10:58:42 +0900

    レンタルグローブの画像差し替え

--- src/components/features/rental/rental.php
+++ src/components/features/rental/rental.php
@@ -10,16 +10,19 @@
   $description = "運動に適した服装でご来場いただければレッスンは可能ですが、お仕事帰りやお出かけ帰りにもご利用いただきやすいようにレンタルシューズもご用意しております。";
   $image = $absoluteUriPath . '/asset/rental/shoes.png';
   $button = null;
-  include $uiPartsPath . '/card/card.php'; ?>
+  include $uiPartsPath . '/card/card.php';
+  ?>
 
   <div class="mt-16"></div>
+
   <?php
   $title = "レンタルグローブ";
   $description = "レンタルグローブは男性用のほかレディースサイズとジュニアサイズもご用意。
   グローブを忘れてしまった、レッスン中に破れてしまったなどのトラブルがあった際はご利用ください。";
   $image = $absoluteUriPath . '/asset/rental/glove.png';
   $button = null;
-  include $uiPartsPath . '/card/card.php'; ?>
+  include $uiPartsPath . '/card/card.php';
+  ?>
 
 </div>
 

a951e15b49af45cc2107bc1116570e0f72495a52
calm1205 <naginani072@gmail.com>
2023-08-13 10:40:54 +0900

    copy light変更

--- src/components/ui_parts/footer/copyright.php
+++ src/components/ui_parts/footer/copyright.php
@@ -1,3 +1,5 @@
   <div class="w-full bg-sky-800 p-2 h-12 flex justify-center items-center">
-    <p class="text-white text-xs text-center font-light">&copy; 2021-2023 ゴルトモアカデミー. All Rights Reserved.</p>
+    <p class="text-white text-xs text-center font-light">
+      &copy; 2021-2023 GOLTOMOACADEMY All Rights Reserved.
+    </p>
   </div>

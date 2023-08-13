79a02e490326fcee846cec3dae5ca383715e2945
calm1205 <naginani072@gmail.com>
2023-08-13 10:37:35 +0900

    案内画像の中央寄せ

--- src/components/ui_parts/guidance/guidance.php
+++ src/components/ui_parts/guidance/guidance.php
@@ -1,13 +1,13 @@
-<div id="guidance" class="flex flex-col text-center mx-auto w-[144px] pt-24 -mt-24">
-  <h2 class="text-2xl font-bold text-center pb-2">
-    営業のご案内
-  </h2>
-  <hr class="w-[100px] border-b border-solid border-[#333] mb-4 text-center mx-auto">
-</div>
+<div class="bg-white">
+  <div id="guidance" class="flex flex-col text-center mx-auto w-[144px] pt-24 -mt-24">
+    <h2 class="text-2xl font-bold text-center pb-2">
+      営業のご案内
+    </h2>
+    <hr class="w-[100px] border-b border-solid border-[#333] mb-4 text-center mx-auto">
+  </div>
 
-
-
-<div class="flex phone:flex-col justify-center gap-5">
-  <img class="w-80" src="<?php echo $absoluteUriPath ?>/asset/guidance/guidance.png" />
-  <?php include 'guidanceTable.php' ?>
+  <div class="flex phone:flex-col justify-center items-center gap-5">
+    <img class="w-80" src="<?php echo $absoluteUriPath ?>/asset/guidance/guidance.png" />
+    <?php include 'guidanceTable.php' ?>
+  </div>
 </div>

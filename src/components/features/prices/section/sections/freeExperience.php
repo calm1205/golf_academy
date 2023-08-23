29afee7306d16ba5dd3bbf44d9df16fc8a4d662a
satohiro213 <satohirosnorlax0213@gmail.com>
2023-08-23 20:41:13 +0900

    "体験レッスンのセクションを追加"

---
+++ src/components/features/prices/section/sections/freeExperience.php
@@ -0,0 +1,15 @@
+
+<?php
+$title = '体験レッスン';
+$description = "最新のトラックマンを活用したレッスンを、無料で体験いただけます！
+
+ゴルトモアカデミーのレッスンが気になる方は、まずはお気軽に体験レッスンにお申し込みください！";
+$table = [
+  "レッスン料金" => ["無料"],
+  "レッスン時間" => ["1回/30分"],
+];
+$note = null;
+$buttonText = "体験レッスンのお申し込みはこちら";
+$buttonLink = "https://coubic.com/goltomo/2185261#pageContent%EF%BC%89%3C/p%3E";
+include dirname(__FILE__) . '/../section.php';
+?>
